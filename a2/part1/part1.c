1bd2841724e086218d0da2316cba8b4f09a70505
Arjun Sheshadri <64653918+GELIOS-19@users.noreply.github.com>
2024-06-09 17:57:43 -0400

    Cleanup

--- a2/part1/part1.c
+++ a2/part1/part1.c
@@ -17,24 +17,22 @@
  *
  *  Use goto statements
  */
-long mathpow(int base, int exp)
-{
-    long result = 1;
-    int i = 0;
+long mathpow(int base, int exp) {
+  long result = 1;
+  int i = 0;
 
 loop: // for (int i = 0; i < exp; i++)
-    if (!(i < exp))
-    {
-        goto ret;
-    }
+  if (!(i < exp)) {
+    goto ret;
+  }
 
-    result *= base;
+  result *= base;
 
-    i++;
-    goto loop;
+  i++;
+  goto loop;
 
 ret:
-    return result;
+  return result;
 }
 
 /**
@@ -55,70 +53,62 @@ ret:
  *
  * YOU MUST USE GOTO statements, no loops, else if, else.
  */
-int main(int argc, char** argv)
-{
-    if (argc < 3)
-    {
-        fprintf(stderr, "./binarytodec {binary string} {1 for signed, 0 for unsigned}");
-        goto ret;
-    }
+int main(int argc, char** argv) {
+  if (argc < 3) {
+    fprintf(stderr, "./binarytodec {binary string} {1 for signed, 0 for unsigned}");
+    goto ret;
+  }
 
-    char* binary_string = argv[1];
-    int is_signed = *argv[2] - 48;
+  char* binary_string = argv[1];
+  int is_signed = *argv[2] - 48;
 
-    long result = 0;
-    char* bit_ptr = binary_string;
+  long result = 0;
+  char* bit_ptr = binary_string;
 
-    int signbit = binary_string[0] - 48;
-    int maxbit;
-    int minbit = 0;
+  int signbit = binary_string[0] - 48;
+  int maxbit;
+  int minbit = 0;
 
 check_binary:
-    if (bit_ptr == binary_string + strlen(binary_string) - 1)
-    {
-        goto check_sign;
-    }
+  if (bit_ptr == binary_string + strlen(binary_string) - 1) {
+    goto check_sign;
+  }
 
-    if (!(*bit_ptr == 48 || *bit_ptr == 49 || *bit_ptr == '\0'))
-    {
-        fprintf(stderr, "invalid binary string");
-        goto ret;
-    }
+  if (!(*bit_ptr == 48 || *bit_ptr == 49 || *bit_ptr == '\0')) {
+    fprintf(stderr, "invalid binary string");
+    goto ret;
+  }
 
-    bit_ptr++;
-    goto check_binary;
+  bit_ptr++;
+  goto check_binary;
 
 check_sign:
-    if (is_signed)
-    {
-        if (signbit == 1)
-        {
-            result += -mathpow(2, strlen(binary_string) - 1);
-        }
-
-        maxbit = strlen(binary_string) - 2;
-        goto convert;
+  if (is_signed) {
+    if (signbit == 1) {
+      result += -mathpow(2, strlen(binary_string) - 1);
     }
 
-    maxbit = strlen(binary_string) - 1;
+    maxbit = strlen(binary_string) - 2;
     goto convert;
+  }
+
+  maxbit = strlen(binary_string) - 1;
+  goto convert;
 
 convert:
-    if (!(minbit <= maxbit))
-    {
-        goto ret;
-    }
+  if (!(minbit <= maxbit)) {
+    goto ret;
+  }
 
-    if (*bit_ptr == 49)
-    {
-        result += mathpow(2, minbit);
-    }
+  if (*bit_ptr == 49) {
+    result += mathpow(2, minbit);
+  }
 
-    minbit++;
-    bit_ptr--; // when we start the loop, bit_ptr = binary_string + strlen(binary_string) - 1
-    goto convert;
+  minbit++;
+  bit_ptr--; // when we start the loop, bit_ptr = binary_string + strlen(binary_string) - 1
+  goto convert;
 
 ret:
-    printf("%li", result);
-    return EXIT_SUCCESS;
+  printf("%li", result);
+  return EXIT_SUCCESS;
 }
