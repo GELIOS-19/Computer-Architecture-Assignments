ea79a67794d8eaa274147223cd1ac5ec5e9b3251
Arjun Sheshadri <64653918+GELIOS-19@users.noreply.github.com>
2024-06-10 21:32:22 -0400

    Added Format

--- a2/part2/part2.c
+++ a2/part2/part2.c
@@ -9,62 +9,62 @@
  * the writeup and autograder for expected values.
  */
 int main(int argc, char** argv) {
-  // 16-bits
-  // 1 sign bit
-  // 5 bit exponent
-  // 10 bit fraction
-  // If the exponent is all 0's, the number is denormalized
-  // If the exponent is all 1's, the number is a special case
-  const int EXP_BIT_SIZE = 5;
+        // 16-bits
+        // 1 sign bit
+        // 5 bit exponent
+        // 10 bit fraction
+        // If the exponent is all 0's, the number is denormalized
+        // If the exponent is all 1's, the number is a special case
+        const int EXP_BIT_SIZE = 5;
 
-  char* binary_string = argv[1];
+        char* binary_string = argv[1];
 
-  char* bit_ptr = binary_string;
+        char* bit_ptr = binary_string;
 
-  long double result = 0;
-  unsigned long binary;
+        long double result = 0;
+        unsigned long binary;
 
 check_binary:
-  if (bit_ptr == binary_string + strlen(binary_string) - 1) {
-    bit_ptr = binary_string + 1;
-    goto check_size;
-  }
+        if (bit_ptr == binary_string + strlen(binary_string) - 1) {
+                bit_ptr = binary_string + 1;
+                goto check_size;
+        }
 
-  if (!(*bit_ptr == 48 || *bit_ptr == 49)) {
-    fprintf(stderr, "invalid format");
-    goto ret;
-  }
+        if (!(*bit_ptr == 48 || *bit_ptr == 49)) {
+                fprintf(stderr, "invalid format");
+                goto ret;
+        }
 
-  bit_ptr++;
-  goto check_binary;
+        bit_ptr++;
+        goto check_binary;
 
 check_size:
-  if (strlen(binary_string) != 16) {
-    fprintf(stderr, "must be 16 bits");
-    goto ret;
-  }
+        if (strlen(binary_string) != 16) {
+                fprintf(stderr, "must be 16 bits");
+                goto ret;
+        }
 
-  goto fpclass;
+        goto fpclass;
 
 fpclass:
-  binary = strtoul(binary_string, NULL, 2);
-  binary = (binary & 0b0111110000000000) >> 10;
+        binary = strtoul(binary_string, NULL, 2);
+        binary = (binary & 0b0111110000000000) >> 10;
 
-  if (binary == 0b11111) {
-    printf("This value is a special case");
-    goto ret;
-  }
+        if (binary == 0b11111) {
+                printf("This value is a special case");
+                goto ret;
+        }
 
-  if (binary == 0b00000) {
-    printf("This value is denormalized");
-    goto ret;
-  }
+        if (binary == 0b00000) {
+                printf("This value is denormalized");
+                goto ret;
+        }
 
-  if (0b00000 < binary && binary < 0b11111) {
-    printf("This value is normalized");
-    goto ret;
-  }
+        if (0b00000 < binary && binary < 0b11111) {
+                printf("This value is normalized");
+                goto ret;
+        }
 
 ret:
-  return EXIT_SUCCESS;
+        return EXIT_SUCCESS;
 }
--- a2/part4/part4.c
+++ a2/part4/part4.c
@@ -3,27 +3,27 @@
 #include <strings.h>
 
 int first_set_bit(int n) {
-  int i = 0;
+        int i = 0;
 
 check_bit:
-  if ((n & (1 << i)) != 0) {
-    return i;
-  }
+        if ((n & (1 << i)) != 0) {
+                return i;
+        }
 
-  i++;
+        i++;
 
-  if (i < 32) {
-    goto check_bit;
-  }
+        if (i < 32) {
+                goto check_bit;
+        }
 
-  return -1;
+        return -1;
 }
 
 int main(int argc, char** argv) {
-  if (argc != 2) {
-    fprintf(stderr, "Usage: ./firstsetbit INTEGER");
-    return 1;
-  }
+        if (argc != 2) {
+                fprintf(stderr, "Usage: ./firstsetbit INTEGER");
+                return 1;
+        }
 
-  printf("%d\n", first_set_bit(atoi(argv[1])));
+        printf("%d\n", first_set_bit(atoi(argv[1])));
 }
